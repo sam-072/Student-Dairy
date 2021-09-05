dbbd5e3c0e2425f5c16639b96da84bdaae39ab7c
sam-072 <shyamprakash807@gmail.com>
2021-09-05 15:47:51 +0530

    login page

---
+++ login.js
@@ -0,0 +1,18 @@
+
+function register() {
+    x.style.left = "-400px";
+    y.style.left = "50px";
+    z.style.left = "110px";
+    w.style.visibility = "hidden";
+    b.style.color = "#fff";
+    a.style.color = "#000";
+  }
+  
+  function login() {
+    x.style.left = "50px";
+    y.style.left = "450px";
+    z.style.left = "0px";
+    w.style.visibility = "visible";
+    a.style.color = "#fff";
+    b.style.color = "#000";
+  }
